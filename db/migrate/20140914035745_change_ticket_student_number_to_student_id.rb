5f8559849bea1f8c17f4b601840ec8778b0273b4
syaffers <syafiqkamarulazman@hotmail.com>
2014-09-15 00:04:52 +0800

    DB fixes

--- db/migrate/20140914035745_change_ticket_student_number_to_student_id.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTicketStudentNumberToStudentId < ActiveRecord::Migration
-  def up
-    rename_column :tickets, :student_number, :student_id
-  end
-
-  def down
-    rename_column :tickets, :student_id, :student_number
-  end
-end
