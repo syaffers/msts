bd836a7e766f7750d98a5601ba712197c32aa527
syaffers <syafiqkamarulazman@hotmail.com>
2014-09-21 17:23:16 +0800

    Event model update to prevent serial number crossing

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,8 @@ class Event < ActiveRecord::Base
 	has_many :tickets, dependent: :destroy
 
 	validates_presence_of :name
-	validates :starting_serial, presence: true, numericality: { only_integer: true, :greater_than => 0 }
-	validates :ending_serial, presence: true, numericality: { only_integer: true, :greater_than => 0 }
+	validates :starting_serial, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 140000, :less_than => :ending_serial }
+	validates :ending_serial, presence: true, numericality: { only_integer: true, :greater_than => :starting_serial, :less_than_or_equal_to => 149999 }
 	validates :serial_number_range, :range => { :not_overlapping => :other_records }
 	validates :member_price, presence: true, numericality: { :greater_than => 0 }
 	validates :non_member_price, presence: true, numericality: { :greater_than => 0 }
